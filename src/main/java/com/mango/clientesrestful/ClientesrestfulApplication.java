62fa37f759b3079d4a1e09134f35ec4509c62852
joyceambrosio <joyceambrosio@users.noreply.github.com>
2018-07-05 15:25:43 -0300

    database config

--- src/main/java/com/mango/clientesrestful/ClientesrestfulApplication.java
+++ src/main/java/com/mango/clientesrestful/ClientesrestfulApplication.java
@@ -2,13 +2,15 @@ package com.mango.clientesrestful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@ComponentScan
 @SpringBootApplication
-public class ClientesrestfulApplication {
+public class ClientesrestfulApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     @ResponseBody
@@ -19,4 +21,9 @@ public class ClientesrestfulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesrestfulApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClientesrestfulApplication.class);
+    }
 }
--- src/main/java/com/mango/clientesrestful/DatabaseConfig.java
+++ src/main/java/com/mango/clientesrestful/DatabaseConfig.java
@@ -7,8 +7,10 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource({ "classpath:application.properties" })
 public class DatabaseConfig {
     @Bean
     @Primary
